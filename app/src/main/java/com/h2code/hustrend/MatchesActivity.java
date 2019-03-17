805b1bdb965264239233b57c66150b14524eb94d
razzazcode <razzazcode@users.noreply.github.com>
2019-03-17 14:59:53 +0200

    new files

---
+++ app/src/main/java/com/h2code/hustrend/MatchesActivity.java
@@ -0,0 +1,13 @@
+package com.h2code.hustrend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MatchesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_matches);
+    }
+}
